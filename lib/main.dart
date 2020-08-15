b602f67026e5d69b7a8ef34e00791e03c92ab60d
morswin22 <xmorswinx@gmail.com>
2020-08-15 21:13:51 +0200

    :sparkles: Add Firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:tankomat/views/Home/HomeView.dart';
+import 'package:firebase/firebase.dart' as fb;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  await DotEnv().load('.env');
+  try {
+    Map<String, String> config = DotEnv().env;
+    fb.initializeApp(
+        apiKey: config['API_KEY'],
+        authDomain: config['AUTH_DOMAIN'],
+        databaseURL: config['DATABASE_URL'],
+        storageBucket: config['STORAGE_BUCKET'],
+        projectId: config['PROJECT_ID'],
+        appId: config['APP_ID'],
+        messagingSenderId: config['MESSAGING_SENDER_ID'],
+        measurementId: config['MEASUREMENT_ID']);
+
+    runApp(MyApp());
+  } on fb.FirebaseJsNotLoadedException catch (e) {
+    print(e);
+  }
+}
 
 class MyApp extends StatelessWidget {
+  final fb.Auth auth;
+
+  MyApp() : auth = fb.auth();
+
   // Root widget of the application
   @override
   Widget build(BuildContext context) {
