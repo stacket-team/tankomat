da580e1a0242ec656458df1a6c2f25b297dcc6dc
morswin22 <xmorswinx@gmail.com>
2020-08-16 14:41:33 +0200

    :recycle: Moved Firebase credentials to constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:tankomat/views/Login/LoginView.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:tankomat/utils.dart';
 
 void main() async {
-  await DotEnv().load('.env');
-  initializeFirebase(DotEnv().env);
+  initializeFirebase();
   runApp(MyApp());
 }
 
--- lib/utils.dart
+++ lib/utils.dart
@@ -1,36 +1,36 @@
+import 'package:tankomat/constants.dart';
 import 'package:firebase/firebase.dart' as firebase;
 
-void initializeFirebase(Map<String, String> config) {
+void initializeFirebase() {
   try {
     if (firebase.apps.isEmpty) {
       firebase.initializeApp(
-          apiKey: config['API_KEY'],
-          authDomain: config['AUTH_DOMAIN'],
-          databaseURL: config['DATABASE_URL'],
-          storageBucket: config['STORAGE_BUCKET'],
-          projectId: config['PROJECT_ID'],
-          appId: config['APP_ID'],
-          messagingSenderId: config['MESSAGING_SENDER_ID'],
-          measurementId: config['MEASUREMENT_ID']);
+        apiKey: API_KEY,
+        authDomain: AUTH_DOMAIN,
+        databaseURL: DATABASE_URL,
+        storageBucket: STORAGE_BUCKET,
+        projectId: PROJECT_ID,
+        appId: APP_ID,
+        messagingSenderId: MESSAGING_SENDER_ID,
+        measurementId: MEASUREMENT_ID,
+      );
     }
     firebase.Auth auth = firebase.auth();
     auth.useDeviceLanguage();
-    auth.onAuthStateChanged
-        .listen(authStateChanged(auth, config['AUTH_DOMAIN']));
+    auth.onAuthStateChanged.listen(authStateChanged(auth));
   } on firebase.FirebaseJsNotLoadedException catch (e) {
     print(e.toString());
   }
 }
 
-Function authStateChanged(firebase.Auth auth, String authDomain) =>
-    (firebase.User user) async {
+Function authStateChanged(firebase.Auth auth) => (firebase.User user) async {
       if (user != null) {
         if (user.emailVerified) {
           // TODO Redirect to Home instead of Register view
         } else {
           try {
             await auth.currentUser.sendEmailVerification(
-                firebase.ActionCodeSettings(url: 'https://$authDomain'));
+                firebase.ActionCodeSettings(url: 'https://$AUTH_DOMAIN'));
           } catch (e) {
             // TODO Display information about email verification
             print(e.toString());
