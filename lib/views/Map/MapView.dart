f6f9de04a3198e4efbd290a75059f2283bd1cdad
k_krylowicz <kacper@krylowicz.pl>
2020-08-22 10:41:30 +0200

    :sparkles: Get acces to camera

--- lib/views/Map/MapView.dart
+++ lib/views/Map/MapView.dart
@@ -1,4 +1,7 @@
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html';
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 
 class MapView extends StatefulWidget {
   @override
@@ -6,12 +9,60 @@ class MapView extends StatefulWidget {
 }
 
 class _MapState extends State<MapView> {
+  Widget _webcamWidget;
+  VideoElement _webcamVideoElement;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _webcamVideoElement = VideoElement();
+
+    // This error occures because of IDE, ignore it (works in build)
+    ui.platformViewRegistry.registerViewFactory(
+      'webcamVideoElement',
+      (int viewId) => _webcamVideoElement,
+    );
+
+    _webcamWidget =
+        HtmlElementView(key: UniqueKey(), viewType: 'webcamVideoElement');
+
+    window.navigator.getUserMedia(video: true).then((MediaStream stream) {
+      _webcamVideoElement.srcObject = stream;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: Container(),
         title: Text('Mapa'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Webcam MediaStream',
+              style: TextStyle(fontSize: 30.0),
+            ),
+            Container(
+              width: 750.0,
+              height: 750.0,
+              child: _webcamWidget,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Start stream, stop stream',
+        child: Icon(Icons.camera_alt),
+        onPressed: () {
+          _webcamVideoElement.srcObject.active
+              ? _webcamVideoElement.play()
+              : _webcamVideoElement.pause();
+        },
       ),
     );
   }
