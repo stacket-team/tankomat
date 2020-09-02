a972a09dc4988c2371a2b870a62155f189c04135
morswin22 <xmorswinx@gmail.com>
2020-09-02 23:05:29 +0200

    :bento: Update name and logo

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Tankomat',
+      title: 'Dopine',
       theme: theme(),
       initialRoute: '/loading',
       onGenerateRoute: RouteGenerator.generateRoute,
